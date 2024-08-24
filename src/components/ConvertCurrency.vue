e9ff6979f702e15e58669ac732488861789e434c
juniorjaviersanchez <juniorsc750@gmail.com>
2024-08-23 21:24:51 -0500

    Add ms

--- src/components/ConvertCurrency.vue
+++ src/components/ConvertCurrency.vue
@@ -1,66 +1,69 @@
 <template>
   <v-container>
-    <v-card
-    class="mx-auto mt-10"
-    width="500"
-    >
-      <template v-slot:title>
-        <v-col cols="12">
-          <v-img
-            :src="logo"
-            class="my-3"
-            contain
-            height="100"
-          />
-        </v-col>
-        <span class="font-weight-black">Conversor de moneda</span>
-      </template>
-
-      <v-card-text>
-        <div class="text-medium-emphasis text-justify">
-          Proyecto N1: Este proyecto utiliza Vue.js para construir la interfaz de usuario, Vuetify para los componentes estilizados, y Axios para realizar peticiones a una API de tasas de cambio. Además, SweetAlert2 se emplea para mostrar alertas personalizadas.
-        </div>
-      </v-card-text>
-
-      <v-card-text class="bg-surface-light pt-4 text-center">
-        <v-form ref="form">
-          <v-col cols="12" md="12">
-            <v-text-field
-              v-model="amount"
-              label="Cantidad"
-              type="number"
-              min="0.01"
-              :rules="amountRules"
-            ></v-text-field>
-          </v-col>
-  
-          <v-col cols="12" md="12">
-            <v-select
-              v-model="fromCurrency"
-              :items="currencies"
-              label="De"
-            ></v-select>
-          </v-col>
-          <v-col cols="12" md="12">
-            <v-select
-              v-model="toCurrency"
-              :items="currencies"
-              label="A"
-            ></v-select>
-          </v-col>
-          <v-col cols="12" md="12">
-            <v-btn color="#622ec3" @click="submit">
-              Convertir
-            </v-btn>
-          </v-col>
-        </v-form>
-        <v-col cols="12">
-          <v-alert v-if="result" type="success">
-            {{ result }} {{ toCurrency }}
-          </v-alert>
-        </v-col>
-      </v-card-text>
-    </v-card>
+    <v-row>
+      <v-col cols="12 mx-auto" sm="8" md="6" lg="5">
+        <v-card
+        class="mx-auto mt-10"
+        >
+          <template v-slot:title>
+            <v-col cols="12">
+              <v-img
+                :src="logo"
+                class="my-3"
+                contain
+                height="100"
+              />
+            </v-col>
+            <span class="font-weight-black">Conversor de moneda</span>
+          </template>
+    
+          <v-card-text>
+            <div class="text-medium-emphasis text-justify">
+              Proyecto N1: Este proyecto utiliza Vue.js para construir la interfaz de usuario, Vuetify para los componentes estilizados, y Axios para realizar peticiones a una API de tasas de cambio. Además, SweetAlert2 se emplea para mostrar alertas personalizadas.
+            </div>
+          </v-card-text>
+    
+          <v-card-text class="bg-surface-light pt-4 text-center">
+            <v-form ref="form">
+              <v-col cols="12" md="12">
+                <v-text-field
+                  v-model="amount"
+                  label="Cantidad"
+                  type="number"
+                  min="0.01"
+                  :rules="amountRules"
+                ></v-text-field>
+              </v-col>
+      
+              <v-col cols="12" md="12">
+                <v-select
+                  v-model="fromCurrency"
+                  :items="currencies"
+                  label="De"
+                ></v-select>
+              </v-col>
+              <v-col cols="12" md="12">
+                <v-select
+                  v-model="toCurrency"
+                  :items="currencies"
+                  label="A"
+                ></v-select>
+              </v-col>
+              <v-col cols="12" md="12">
+                <v-btn color="#622ec3" @click="submit">
+                  Convertir
+                </v-btn>
+              </v-col>
+            </v-form>
+            <v-col cols="12">
+              <v-alert v-if="result" type="success">
+                {{ result }} {{ toCurrency }}
+              </v-alert>
+            </v-col>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
